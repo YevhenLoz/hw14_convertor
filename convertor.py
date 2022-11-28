c9f5b637e8bea41c85478a6d8fe6dfda89622097
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-11-28 23:29:00 +0200

    update

--- convertor.py
+++ convertor.py
@@ -1,6 +1,9 @@
 import json
 from argparse import ArgumentParser
+from xml.dom.minidom import parseString
+from dicttoxml import dicttoxml
 from xml_marshaller import xml_marshaller
+
 
 
 class Human:
@@ -13,23 +16,29 @@ class Human:
     @staticmethod
     def convert_to_json():
         human = Human("John", 15, "man", 2003)
-        to_json = json.dumps(human.__dict__)
-        with open("human.txt", "w") as file:
-            json.dump(to_json, file)
+        to_json = json.dumps(vars(human))
+        to_file = json.loads(to_json)
+        with open("human.json", "w") as file:
+            json.dump(to_file, file, indent=4)
 
     @staticmethod
     def convert_to_xml():
         human = Human("John", 15, "man", 2003)
-        xml_from_class = xml_marshaller.dumps(human.__dict__)
-        with open("human.xml", "wb") as file:
-            xml_marshaller.dump(xml_from_class, file)
+        xml_from_class = xml_marshaller.dumps(vars(human))
+        xml_data = xml_marshaller.loads(xml_from_class)
+        xml = dicttoxml(xml_data, attr_type=False)
+        xml_decode = xml.decode()
+        xml_format = parseString(xml_decode).toprettyxml()
+        xml_file = open("human.xml", "w")
+        xml_file.write(xml_format)
+        xml_file.close()
 
 
 parser = ArgumentParser(description="Converter")
-parser.add_argument('--convert', help='Converts to json or xml', default='xml to cli')
+parser.add_argument('--convert', help='Converts to json or xml', default='jsontocli')
 arguments = parser.parse_args()
 
-if arguments.convert == 'json to cli':
+if arguments.convert == 'jsontocli':
     Human.convert_to_json()
-elif arguments.convert == 'xml to cli':
+elif arguments.convert == 'xmltocli':
     Human.convert_to_xml()
